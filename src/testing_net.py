1df9b066d98c42a753819338dd73d7ec5d5a66f0
GiannisChouliaras <giannis.chouliaras10@gmail.com>
2022-03-26 21:15:22 +0200

    Change: values of testing_net.py

--- src/testing_net.py
+++ src/testing_net.py
@@ -1,27 +1,27 @@
 import numpy as np
 import torch
-from func import convert_number, array_to_tensor
-from net import Net
+
+from func import array_to_tensor
+from net_crossentropy import SoftNet
 
 
 def main() -> None:
     # load model
-    model = Net(inFeats=3, outFeats=1, fHidden=10, sHidden=5)
-    model.load_state_dict(torch.load("../models/net.pt"))
+    model = SoftNet(inputs=3, outputs=8, hidden_size=10)
+    model.load_state_dict(torch.load("../models/net_ce.pt"))
     model.eval()
 
-    F1 = 0.7322
-    F2 = 0.3929
-    F3 = 0.5050
-    actual = 0.659
+    F1 = 0.2970
+    F2 = 0.5880
+    F3 = 0.5270
+    actual = 7
 
     data = np.array([F1, F2, F3])
     data = array_to_tensor(data)
 
-    prediction = model(data).item()
-    print(round(prediction, 3))
-    print(f"Result is: {convert_number(number=round(prediction, 3), range1=(0, 1), range2=(1, 8))}")
-    print(f"actual is: {convert_number(number=actual, range1=(0, 1), range2=(1, 8))}")
+    prediction = model(data)
+    print(f"actual is {actual} and prediction is :", end=" ")
+    print(torch.argmax(prediction).item() + 1)
 
 
 if __name__ == "__main__":
